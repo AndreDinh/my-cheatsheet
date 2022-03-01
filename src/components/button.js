0b1e56660054c43a0ff90ddb4e69cefe5c3a3e38
Andre Dinh <dinh.andre@yahoo.com>
2022-02-28 17:05:49 -0800

    added a counter for now

--- src/components/button.js
+++ src/components/button.js
@@ -3,10 +3,11 @@ import {useState} from 'react';
 
 function Button (props ){
     const [authorized, clickedAuthorized] = useState(false);
-    
+    const counter = 1;
     const authorizedHandler = () => {
         clickedAuthorized(true);
         console.log('you are authorized');
+        counter++;
     };
     return(
         <>
